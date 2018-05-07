8120c453ce4aae71b2737a2ab4f5221cd0cb90da
Danuta <danprzy@gmail.com>
2018-05-07 15:31:49 +0200

    Add basic scripts

---
+++ scripts.js
@@ -0,0 +1,17 @@
+var url = 'http://api.icndb.com/jokes/random';
+var paragraph = document.getElementById('joke');
+
+var button = document.getElementById('get-joke');
+button.addEventListener('click', function () {
+  getJoke();
+});
+
+function getJoke() {
+  var request = new XMLHttpRequest();
+    request.open('GET', url);
+    request.addEventListener('load', function () {
+        var response = JSON.parse(request.response);
+        paragraph.innerHTML = response.value.joke;
+    });
+    request.send();
+}
