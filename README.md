9a9fb3e3a77b275f08f2f60a8afc11287dd54d81
Danuta Przydrozna <danprzy@gmail.com>
2018-07-11 15:34:16 +0200

    Add readme and change style.css

---
+++ README.md
@@ -0,0 +1,17 @@
+# AJAX-Random-Joke
+> Chuck Norris Random Joke Generator - exercise for Kodilla bootcamp.
+
+## Live Demo
+https://danprzy.github.io/12.4-AJAX-Random-Joke/
+
+## Technologies
+* HTML5
+* CSS3
+* JavaScript
+* jQery
+* AJAX
+
+## How does it work?
+Pressing the "Show joke" button uses AJAX to request gets a JSON response from ICNDb.
+
+
--- scripts.js
+++ scripts.js
@@ -2,25 +2,25 @@ var url = 'http://api.icndb.com/jokes/random';
 var paragraph = document.getElementById('joke');
 
 var button = document.getElementById('get-joke');
-button.addEventListener('click', function () {
-  getJoke();
+button.addEventListener('click', function() {
+    getJoke();
 });
 
 function getJoke() {
-  var request = new XMLHttpRequest();
+    var request = new XMLHttpRequest();
     request.open('GET', url);
-    request.addEventListener('load', function () {
+    request.addEventListener('load', function() {
         var response = JSON.parse(request.response);
         paragraph.innerHTML = response.value.joke;
     });
     request.send();
 }
 //load joke at the end of the document loading process
-window.onload = function () {
-  getJoke();
+window.onload = function() {
+    getJoke();
 };
 
-//jQery
+//jQuery
 /*
 $(document).ready(function(){
  var url = 'http://api.icndb.com/jokes/random';
